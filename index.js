d44e3ebd3c19f7436a5f7078023f1d0ed1590859
AnujKaran <128172283+AnujKaran@users.noreply.github.com>
2024-05-21 23:27:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://conversify-frontend-six.vercel.app/",
+    origin: "*",
     credentials: true,
   },
 });
